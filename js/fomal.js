78cdedd0a38527ebb3886e75d76adf1b3521bac2
staywuqi <wq1447728895@gmail.com>
2023-08-29 16:22:35 +0800

    Site updated: 2023-08-29 16:22:34

--- js/fomal.js
+++ js/fomal.js
@@ -3174,10 +3174,10 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
-    --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
-    --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
-    --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
+    --default-bg: url(https://staywuqi.github.io/assets/back.jpg);
+    --darkmode-bg:url(https://staywuqi.github.io/assets/back.jpg);
+    --mobileday-bg: url(https://staywuqi.github.io/assets/back.jpg);
+    --mobilenight-bg: url(https://staywuqi.github.io/assets/back.jpg);
   }`;
 }
 // 切换背景主函数
